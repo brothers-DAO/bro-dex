5dcfe507244269a7b58b450edbcfc5ada64d2bab
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2025-03-04 21:43:29 +0100

    Better manage unexistant accounts

--- basic-frontend/src/backend/tokens-react.js
+++ basic-frontend/src/backend/tokens-react.js
@@ -4,6 +4,9 @@ import {to_decimal} from './utils.js';
 const NETWORK = import.meta.env.VITE_NETWORK
 const CHAIN = import.meta.env.VITE_CHAIN
 
+const K_ACCOUNT_REGEXP = /k:([0-9a-f]{64})/
+
+
 export function useTokenBalance(account, token)
 {
   const code = (account && token )?`(try 0.0 (${token}.get-balance ${account})`:null;
@@ -38,17 +41,24 @@ export function useSingleKeyAccount(account)
 {
   const {guard, error} = useGuard(account);
 
+  const k_match = account && account.match(K_ACCOUNT_REGEXP)
+
   if(guard)
   {
     if(guard?.pred == "keys-all" && guard?.keys.length == 1)
       return {key:guard.keys[0], error}
+    else if(Object.keys(guard).length === 0 && k_match) /*Non existing account */
+    {
+      console.log("Extracting key from missing k:account")
+      return {key:k_match[1], error:null}
+    }
     else
-      {
-        console.warn("Unsupported guard:")
-        console.warn(guard)
-        return {key:null, error}
-      }
+    {
+      console.warn("Unsupported guard:")
+      console.warn(guard)
+      return {key:null, error}
+    }
   }
-  else
-    return {key:null, error}
+
+  return {key:null, error}
 }
