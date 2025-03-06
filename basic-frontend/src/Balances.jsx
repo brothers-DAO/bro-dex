fee3b7bc64843e37f4aee6c4a70a4a2a57eb66fc
CryptoPascal31 <CryptoPascal31@users.noreply.github.com>
2025-03-06 17:40:52 +0100

    Show more token details in balance

--- basic-frontend/src/Balances.jsx
+++ basic-frontend/src/Balances.jsx
@@ -1,25 +1,60 @@
-import {useContext, useEffect} from 'react';
+import {useContext, useEffect, useRef} from 'react';
 import {useTokensBalance} from './backend/tokens-react';
 import { Card } from 'primereact/card';
+import { OverlayPanel } from 'primereact/overlaypanel';
 import { TransactionContext} from './TransactionContext';
+
 import { AccountContext} from './AccountContext';
 import {AccountLink} from './Explorer'
 import {useTokenInfo} from './backend/tokens_info';
+
+const SOCIAL_ICONS = {website:"pi-globe", twitter:"pi-twitter", discord:"pi-discord", github:"pi-github", telegram:"pi-telegram"}
+
+function SocialLink({lnk})
+{
+  const icon = SOCIAL_ICONS[lnk.type] || "pi-external-link";
+  return <a href={lnk.url} style={{ fontSize: '1rem' }} target="_blank" rel="noopener noreferrer" className={"no-underline pi " + icon}/>
+}
+
+function TokenInfoIcon({module})
+{
+  const {token_info} = useTokenInfo(module);
+  console.log(token_info)
+  const op = useRef(null);
+  return  <>
+          {token_info && <img src={token_info.img} className="w-5rem cursor-pointer"  onClick={(e) =>op.current.toggle(e)}/> }
+          <OverlayPanel ref={op} className="order-1 shadow-8 border-1">
+            {token_info &&  <div className="flex flex-column">
+                              <div className="flex flex-row ">
+                                <img src={token_info.img} className="w-3rem"/>
+                                <div className="flex-grow-1 text-center text-3xl align-content-center font-bold">{token_info.name}</div>
+                              </div>
+                              <div className="m-2 font-italic" > {token_info.description} </div>
+                              <div className="m-2 flex flex-column gap-1" >
+                                <div> <span className="font-bold vertical-align-middle">Module:</span> <span className="text-xs vertical-align-middle"> {module}</span></div>
+                                {token_info?.totalSupply && <div> <span className="font-bold">Total supply:</span> {token_info.totalSupply.toString()}</div>}
+                                {token_info?.circulatingSupply && <div> <span className="font-bold">Circulating supply:</span> {token_info.circulatingSupply.toString()}</div>}
+                                <div className="flex flex-row gap-2"> <span className="font-bold">Links:</span>  {token_info.socials.map(x => <SocialLink lnk={x}/>)} </div>
+                              </div>
+                            </div>
+
+            }
+          </OverlayPanel>
+          </>
+}
 
 function Balances({pair})
 {
   const {account} = useContext(AccountContext);
   const {trxCount} = useContext(TransactionContext);
-  const {balance_a, balance_b, mutate} = useTokensBalance(account, pair.quote_module, pair.base_module);
-  const {token_info:base_info} = useTokenInfo(pair.base_module);
-  const {token_info:quote_info} = useTokenInfo(pair.quote_module);
+  const {balances:[balance_a, balance_b], mutate} = useTokensBalance(account, pair.quote_module, pair.base_module);
 
   useEffect(() => {if(trxCount){console.log("Trigger mutation");mutate()}}, [trxCount, mutate])
 
   return <>
           <div className="font-bold text-primary text-center mb-1"> Balances </div>
           <div className="flex flew-row gap-3 text-center justify-content-center align-items-center">
-          {base_info && <img src={base_info.img} className="w-5rem"/> }
+          <TokenInfoIcon module={pair.base_module} />
           <Card title={<div className="flex gap-1 justify-content-center"> {pair.base}<AccountLink account={account} fungible={pair.base_module} /> </div>} className="w-7rem border-2 border-round-lg shadow-4 border-green-600" pt={{body:{className:"p-1"}}} >
             {balance_b?balance_b.toFixed(pair.base_decimals):"~"}
           </Card>
@@ -27,9 +62,11 @@ function Balances({pair})
           <Card title={<div className="flex gap-1 justify-content-center"> {pair.quote}<AccountLink account={account} fungible={pair.quote_module} /> </div>} className="w-7rem border-2 border-round-lg shadow-4 border-red-600" pt={{body:{className:"p-1"}}}>
             {balance_a?balance_a.toFixed(pair.quote_decimals):"~"}
           </Card>
-          {quote_info && <img src={quote_info.img} className="w-5rem"/> }
+          <TokenInfoIcon module={pair.quote_module} />
         </div>
         </>
 }
 
 export default Balances
+
+export {TokenInfoIcon}
